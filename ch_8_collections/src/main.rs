64dd508c7a5b2dee14216cb7af0bd467635aa540
Joseph Ensminger <joetensminger@gmail.com>
2023-01-17 18:23:14 -0600

    playing w/ hashmaps

--- ch_8_collections/src/main.rs
+++ ch_8_collections/src/main.rs
@@ -1,24 +1,69 @@
+
+use std::collections::HashMap;
+// Playing w/ Hash Maps
+fn main() {
+    let mut h: HashMap<char, Vec<usize>> = HashMap::new();
+
+    for (i, c) in "hello!".chars().enumerate() {
+        h.entry(c)
+        .or_insert(Vec::new())
+        .push(i);
+    }
+
+    let mut sum = 0;
+
+    for i in h.get(&'l').unwrap() {
+        sum += *i;
+    }
+
+    println!("{}", sum);
+  }
+
+// Hash Map basics
+// fn main () {
+//     // initializing a Hash Map
+//     let mut scores: HashMap<String, i32> = HashMap::new();
+
+//     // adding k,v pairs to the initialized Hash Map
+//     scores.insert(String::from("Yellow"), 50);
+
+//     // Retrieving a reference to a value from a given key
+//     let score = scores.get(&String::from("Blue")).unwrap_or(&0);
+
+//     // adding a key value pair if the key doesn't already exist
+//     let mut blue_score = scores.entry(String::from("Blue")).or_insert(50);
+//     *blue_score += 5;
+//     println!("{}", blue_score);
+
+//     // iterate over each key/value pair in a Hash Map
+//     for (key, value) in &scores {
+//         println!("{}: {}", key, value)
+//     }
+
+// }
+
+
+
 // Playing w/ Strings
+// fn main () {
+//     let s1 = String::from("नHello, ");
+//     let s2 = String::from("world!");
+//     let s3 = s1 + &s2[..2] + " woah brah"; // note s1 has been moved here and can no longer be used
 
-fn main () {
-    let s1 = String::from("नHello, ");
-    let s2 = String::from("world!");
-    let s3 = s1 + &s2[..2] + " woah brah"; // note s1 has been moved here and can no longer be used
+//     // indexing w/ a range to create a string slice containing particular bytes:
+//     println!("s3 index 0 is {}", &s3[0..3]);
 
-    // indexing w/ a range to create a string slice containing particular bytes:
-    println!("s3 index 0 is {}", &s3[0..3]);
+//     // Iterating Over Strings of Unicode Scalar Values that return chars
+//     for c in s3.chars() {
+//         println!("{}", c);
+//     }
 
-    // Iterating Over Strings of Unicode Scalar Values that return chars
-    for c in s3.chars() {
-        println!("{}", c);
-    }
+//     // Iterating Over Strings of Unicode Scalar Values that return bytes
+//     for b in s3.bytes() {
+//         println!("{}", b);
+//     }
 
-    // Iterating Over Strings of Unicode Scalar Values that return bytes
-    for b in s3.bytes() {
-        println!("{}", b);
-    }
-
-}
+// }
 
 
 // Using enums to store multiple types and iterating through them
