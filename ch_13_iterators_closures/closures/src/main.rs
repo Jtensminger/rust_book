2dded0d040f33d589ae147ba69687298416c8a05
Joseph Ensminger <joetensminger@gmail.com>
2023-01-30 14:01:48 -0600

    playing w/ closures

---
+++ ch_13_iterators_closures/closures/src/main.rs
@@ -0,0 +1,20 @@
+use std::thread;
+
+
+fn main() {
+	// let example_closure = |x| x;
+	// println!("{}", example_closure(3));
+
+
+	// let mut list = vec![1, 2, 3];    
+	// let only_borrows = || println!("From only borrow closure: {:?}", list);
+	// let mut borrows_mutably = || list.push(7);
+
+	// let list = vec![1, 2, 3];
+    	// println!("Before defining closure: {:?}", list);
+
+   	// thread::spawn(move || println!("From thread: {:?}", list))
+        // 	.join()
+        // 	.unwrap();
+
+}
